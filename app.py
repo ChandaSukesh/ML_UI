c3078c21a32393ba25e73a6bdb702ac419ee5c01
ChandaSukesh <71482263+ChandaSukesh@users.noreply.github.com>
2021-09-28 23:31:16 +0530

    Version 3
    Version 3

--- app.py
+++ app.py
@@ -1,8 +1,9 @@
-from flask import Flask,render_template,request
+from flask import Flask,render_template,request,session
 
 import pandas as pd
 
-app = Flask(__name__) #creating the Flask class object   
+app = Flask(__name__) #creating the Flask class object 
+app.secret_key = "any random string"
  
 @app.route("/") #decorator drfines the   
 def index():  
@@ -13,11 +14,32 @@ def index():
 def textfile():
     filename=request.form.get("filename")
     df = pd.read_csv(filename,header=None)
+    session["df"]=df.values.tolist()
     return render_template('ClassReg.html',reads=df.values.tolist())
 
 @app.route('/MainFunction', methods = ['GET','POST'])
 def MainFunction():
-    return render_template('ClassReg.html')
+    df=session["df"]
+    print(df)
+    # filename=request.form.get("")
+    return render_template('BasicFunctions.html')
+
+@app.route('/showData', methods = ['GET','POST'])
+def showData():
+    type=request.form.get("id")
+    df=session["df"]
+    # converting list to dataframe
+    df=pd.DataFrame(df) 
+    if(type=="Head"):
+        content=df.head()
+        message="The below table displays the top 5 rows in a given dataset"
+    if(type=="Describe"):
+        content=df.describe()
+        message="The below table displays entire rows in a given dataset"
+    # if(type=="Predict"):
+    #     message="Please select the Learning rate and Epochs values of your choice"
+    return render_template('BasicFunctions.html', h=content.values.tolist(),message=message)
+    # 
 
 if __name__ =='__main__':  
     app.run(debug = True)
